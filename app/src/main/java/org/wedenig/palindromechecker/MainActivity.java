e98fa540b830b7c8ba220b4c628da75728659282
Christoph Wedenig <christoph@wedenig.org>
2018-03-06 14:53:06 +0100

    Changed input error handling
    and added emojis 😊

--- app/src/main/java/org/wedenig/palindromechecker/MainActivity.java
+++ app/src/main/java/org/wedenig/palindromechecker/MainActivity.java
@@ -2,6 +2,7 @@ package org.wedenig.palindromechecker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,14 +26,14 @@ public class MainActivity extends AppCompatActivity {
         checkButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (input.getText().toString().length() == 0) {
-                    resultView.setText("Please enter a string.");
+                if (TextUtils.isEmpty(input.getText())) {
+                    input.setError("Input cannot be empty");
                 } else if (input.getText().toString().length() < 5) {
-                    resultView.setText("String needs to be longer than 5 characters.");
+                    input.setError("Input needs to be longer than 5 characters.");
                 }else if(Utility.isPalindrome(input.getText().toString())) {
-                    resultView.setText("It's a palindrome :)");
+                    resultView.setText("'" + input.getText().toString() + "' is a palindrome \uD83D\uDE0A");
                 } else {
-                    resultView.setText("Not a palindrome :(");
+                    resultView.setText("'" + input.getText().toString() + "' is not a palindrome \uD83D\uDE14");
                 }
             }
         });
--- app/src/test/java/org/wedenig/palindromechecker/ExampleUnitTest.java
+++ app/src/test/java/org/wedenig/palindromechecker/ExampleUnitTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * Simple unit tests
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
